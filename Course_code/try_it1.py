bb4393b7bcf0d880a2ef4f55d0e748eed57bce2e
GideonMissoy <giddykips2030@gmail.com>
2023-05-15 00:30:37 +0300

    Try it quiz.

---
+++ Course_code/try_it1.py
@@ -0,0 +1,12 @@
+# Write addition, subtraction, multiplication, and division operations
+# that each result in the number 8
+print(5+3)
+print(12-4)
+print(4*2)
+print(16/2)
+
+# Use a variable to represent your favorite number. Then using that var,
+# create a message that reveals you fav no. Print the message
+favorite_number = 9
+message = f"My favorite number is {favorite_number}"
+print(message)

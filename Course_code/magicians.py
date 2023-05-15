0d5a085bcb580a25ab600299a7ae3e306380e34d
GideonMissoy <giddykips2030@gmail.com>
2023-05-15 10:19:42 +0300

    Looping through lists.

---
+++ Course_code/magicians.py
@@ -0,0 +1,8 @@
+# Introducing python's for loop
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")

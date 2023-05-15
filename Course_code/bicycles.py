49c6b15a88782bebc84129dc3293f4e5d5011caa
GideonMissoy <giddykips2030@gmail.com>
2023-05-15 03:52:41 +0300

    Lists. Manipulating, accessing, inserting, removing, popping, and appending items in a list.

--- Course_code/bicycles.py
+++ Course_code/bicycles.py
@@ -6,3 +6,41 @@ print(bicycles[0].title()) #Manipulating accessed element using methods
 
 message = f"My first bicycle was a {bicycles[2].title()}"
 print(message) #Using f-strings to create a message based on an individual value from list
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
+#Adding a new element at the end of the list
+motorcycles.append('honda')
+print(motorcycles)
+
+#Inserting a new element at any position in the list
+motorcycles.insert(0, 'BMW')
+print(motorcycles)
+
+# Removing an item from a list
+del motorcycles[3]
+print(motorcycles)
+
+#Removing an item using the pop() method
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'BMW', 'Ducati']
+last_owned = motorcycles.pop()
+print(f"The last motorcycle I owned was a {last_owned.title()}")
+first_owned = motorcycles.pop(0)
+print(f"The first motorcyle I owned was a {first_owned.title()}")
+
+#Removing an item when you don't know its index
+motorcycles = ['honda', 'yamaha', 'suzuki', 'BMW', 'Ducati']
+motorcycles.remove('suzuki')
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'BMW', 'Ducati']
+too_expensive = 'Ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")

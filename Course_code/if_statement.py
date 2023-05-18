1eacf9986e8b738431a7576f6da00341d64b4784
GideonMissoy <giddykips2030@gmail.com>
2023-05-18 05:40:42 +0300

    Exploring the different kinds of if statements.

--- Course_code/if_statement.py
+++ Course_code/if_statement.py
@@ -12,3 +12,60 @@ if age >= 18:
 else:
     print("\nSorry, you are too young to vote.")
     print("Please register to vote as soon as you turn 18!")
+
+# The if-elif-else chain.
+age = 12
+if age < 4:
+    print("\nYour admission cost is $0.")
+elif age < 18:
+    print("\nYour admission cost is $25.")
+else:
+    print("\nYour admission cost is $40.")
+
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+
+print(f"Your admission cost is ${price}")
+
+#Using multiple elif blocks
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+
+print(f"\nYour admission cost is ${price}")
+
+#Omitting the else block
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+elif age >= 65:
+    price = 20
+
+print(f"\nYour admission cost is ${price}\n")
+
+# Testing multiple conditions using a series of simple if statements
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")

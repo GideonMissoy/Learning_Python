ad334f2cd90b0b4290803fc9f59225159f31ec28
GideonMissoy <giddykips2030@gmail.com>
2023-05-24 23:41:01 +0300

    Using continue in a loop.

--- Course_code/while_loops.py
+++ Course_code/while_loops.py
@@ -42,3 +42,14 @@ while True:
         break
     else:
         print(f"I'd love to go to {city.title()}")
+
+
+# Using continue in a loop.
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    
+    print(current_number)
+

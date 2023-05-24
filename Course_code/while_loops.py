52de29d5f37f48a4d331aed6c064e903e53e517f
GideonMissoy <giddykips2030@gmail.com>
2023-05-24 23:32:40 +0300

    Using a flag in a while loop.

--- Course_code/while_loops.py
+++ Course_code/while_loops.py
@@ -13,4 +13,21 @@ while message != 'quit':
     message = input(prompt)
     
     if message != 'quit':
+        print(message) #The if statement ensures only the message is printed, 'quit' is not
+
+
+# Using a flag
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+
+while active:
+    message = input(prompt)
+    
+    if message == 'quit':
+        active = False
+    else:
         print(message)
+
+

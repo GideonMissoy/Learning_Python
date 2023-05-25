8c3c439d4cc6a718e7e6ec6055dd8249955fd780
GideonMissoy <giddykips2030@gmail.com>
2023-05-25 03:52:07 +0300

    Introduction to functions and passing information to a function.

--- Course_code/Functions/function1.py
+++ Course_code/Functions/function1.py
@@ -4,3 +4,11 @@ def greet_user():
     print("Hello!")
 
 greet_user()
+
+
+# Passing information to a function.
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+greet_user('jesse')

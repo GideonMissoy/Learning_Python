2bf42b4b73ed418400173c9bd44c45002f839c27
GideonMissoy <giddykips2030@gmail.com>
2023-06-01 23:42:41 +0300

    Passing an arbitrary number of arguments.

--- Course_code/Functions/function_tuples.py
+++ Course_code/Functions/function_tuples.py
@@ -6,3 +6,14 @@ def make_pizza(*toppings):
 
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+# Replacing the print() call with a loop that runs through the list of toppings
+# and describes the pizza being ordered.
+def make_pizza(*toppings):
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"\t- {topping.title()}")
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')

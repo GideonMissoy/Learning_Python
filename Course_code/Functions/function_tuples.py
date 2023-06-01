960246ca28607bec7885ef54d6214a757f8a16e3
GideonMissoy <giddykips2030@gmail.com>
2023-06-01 23:30:32 +0300

    Passing an arbitrary number of argumments.

---
+++ Course_code/Functions/function_tuples.py
@@ -0,0 +1,8 @@
+# Passing an arbitrary number of arguments to a function.
+# Returns a tuple
+def make_pizza(*toppings):
+    """Print the list of toppings that have been requested."""
+    print(toppings)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')

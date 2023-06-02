0ee0bbef798e11824dfcca04d171a7d2481c9cd4
GideonMissoy <giddykips2030@gmail.com>
2023-06-02 23:15:35 +0300

    This file imports the module pizza.py.

---
+++ Course_code/Functions&_Modules/making_pizzas.py
@@ -0,0 +1,5 @@
+import pizza #This line tells python to open the file pizza.py,
+# and copy all the functions from it into this program
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')

a15cbecfb899e1c9b020ca03a5c7744897db94bb
GideonMissoy <giddykips2030@gmail.com>
2023-06-02 23:26:57 +0300

    Importing an entire module vs imporing specific functions.

--- Course_code/Functions&_Modules/making_pizzas.py
+++ Course_code/Functions&_Modules/making_pizzas.py
@@ -3,3 +3,11 @@ import pizza #This line tells python to open the file pizza.py,
 
 pizza.make_pizza(16, 'pepperoni')
 pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+# Importing a specific module.
+from pizza import make_pizza # With this syntax, you don't need to use the dot notation
+# when you call a function. Because we've explicitly imported the function make_pizza()
+# in the import statement, we can call it by name when we use the function.
+
+make_pizza(16, 'pepperoni')

da81069613bba18876d9fa0c2b1c8241d331da9d
GideonMissoy <giddykips2030@gmail.com>
2023-06-06 02:03:14 +0300

    Adding comments.

--- Course_code/Classes/dog.py
+++ Course_code/Classes/dog.py
@@ -14,13 +14,13 @@ class Dog:
         """Simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
 
-
+# Creating instances
 my_dog = Dog('Willie', 6)
 your_dog = Dog('Lucy', 3)
 
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
-my_dog.sit()
+my_dog.sit() # Calling methods
 
 print(f"\nYour dog's name is {your_dog.name}.")
 print(f"Your dog is {your_dog.age} years old.")
